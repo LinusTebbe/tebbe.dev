d32e25219f265d6fe3ba735f957a3b8fd792be33
LinusTebbe <linus@tebbe.dev>
2020-01-18 18:33:38 +0100

    Add legal stuff

---
+++ pages/impressum.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div class="shadow bg-white p-5">
+        <div class="text-4xl">Impressum</div>
+
+        <div class="text-lg">
+            <div class="my-4">
+                <div class="text-2xl">Angaben gemäß § 5 TMG</div>
+                Linus Tebbe, freiberuflicher Programmierer<br>
+                Buxtrup 12<br>
+                48301 Nottuln
+            </div>
+
+            <div class="my-4">
+                <div class="text-2xl">Kontakt</div>
+                Telefon: 025909400811<br>
+                E-Mail: linus@tebbe.dev
+            </div>
+
+            <div>
+                <div class="text-2xl">Streitschlichtung</div>
+                Die Europ&auml;ische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit: <a href="https://ec.europa.eu/consumers/odr" class="hover:underline" target="_blank" rel="noopener noreferrer">https://ec.europa.eu/consumers/odr</a>.<br />
+                Unsere E-Mail-Adresse finden Sie oben im Impressum.<br>
+                <br>
+                Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.
+            </div>
+        </div>
+        <div class="text-gray-600">Quelle: <a href="https://www.e-recht24.de" target="_blank" rel="noopener noreferrer">eRecht24</a></div>
+        </div>
+    </div>
+</template>
