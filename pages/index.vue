21ebddde758fe872e8a2884071c66cfc8a325669
LinusTebbe <linus@tebbe.dev>
2020-07-01 12:41:19 +0200

    Add some technologies

--- pages/index.vue
+++ pages/index.vue
@@ -8,16 +8,18 @@
           <br>
           Profitieren Sie von meiner Jahrelangen Erfahrung im Bereich der Webentwicklung.<br>
           Sei es eine komplette Webplattform, oder doch nur ein recht simples Kommandozeilen-Tool, ich kann Ihnen behilflich sein!<br>
-          Kontaktieren Sie mich, und ich bin mir sicher, dass ich Ihnen ein gutes Angebot machen kann
+          Kontaktieren Sie mich, und ich bin mir sicher, dass ich Ihnen ein gutes Angebot machen kann!<br>
         </Card>
       </div>
       <div class="w-full pt-4 md:pt-0 md:w-1/2 md:pl-2">
         <Card class="h-full">
           <h1>Benutzte Technologien</h1>
           SQL (MySQL, MSSQL, etc.)<br>
-          HTML, CSS<br>
-          JS, Node.JS, Vue.JS, Nuxt, TailwindCSS<br>
+          HTML<br>
+          CSS, TailwindCSS, Bootstrap, UIKit<br>
+          JS, Node.JS, Vue.JS, Nuxt<br>
           PHP, Laravel (Zero), Symfony<br>
+          Wordpress, Shopware<br>
           Flutter, Java, Python<br>
           Linus, Windows, Android<br>
         </Card>
