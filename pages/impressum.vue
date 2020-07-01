33db2c83747757b87619da90b77caecc4a727d3b
LinusTebbe <linus@tebbe.dev>
2020-07-01 14:23:04 +0200

    Fix impressum

--- pages/impressum.vue
+++ pages/impressum.vue
@@ -1,69 +1,64 @@
 <template>
-    <div>
-        <div class="shadow bg-white p-5">
-        <div class="text-4xl">Impressum</div>
+    <Card>
+        <h1>Impressum</h1>
 
-        <div class="text-lg">
-            <div class="my-4">
-                <div class="text-2xl">Angaben gemäß §5 TMG</div>
-                Linus Tebbe, freiberuflicher Programmierer<br>
-                Buxtrup 12<br>
-                48301 Nottuln
-            </div>
-
-            <div class="my-4">
-                <div class="text-2xl">Kontakt</div>
-                Telefon: 02509-2094947<br>
-                E-Mail: linus@tebbe.dev
-            </div>
-
-            <div class="my-4">
-                <div class="text-2xl">Umsatzsteuer-ID</div>
-                UStID. gemäß §27a UStG.: DE326934606
-            </div>
-
-            <div class="my-4">
-                <div class="text-2xl">Streitschlichtung</div>
-                Die Europ&auml;ische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit: <a href="https://ec.europa.eu/consumers/odr" class="hover:underline" target="_blank" rel="noopener noreferrer">https://ec.europa.eu/consumers/odr</a>.<br />
-                Unsere E-Mail-Adresse finden Sie oben im Impressum.<br>
-                <br>
-                Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.
-            </div>
-
-            <div class="my-4">
-                <div class="text-2xl">Haftung für Inhalte</div>
-                Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich.<br>
-                Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.<br>
-                Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt.<br>
-                Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich.<br>
-                Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
-            </div>
-
-            <div class="my-4">
-                <div class="text-2xl">Haftung für Links</div>
-                Unser Angebot enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben.<br>
-                Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen.<br>
-                Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich.<br>
-                Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft.<br>
-                Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.<br>
-                Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar.<br>
-                Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen
-            </div>
-
-            <div class="my-4">
-                <div class="text-2xl">Urheberrecht</div>
-                Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht.<br>
-                Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers.<br>
-                Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.<br>
-                Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet.<br>
-                Insbesondere werden Inhalte Dritter als solche gekennzeichnet.<br>
-                Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis.<br>
-                Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.<br>
-            </div>
+        <div class="my-4">
+          <h2>Angaben gemäß §5 TMG</h2>
+          Linus Tebbe, freiberuflicher Programmierer<br>
+          Buxtrup 12<br>
+          48301 Nottuln
         </div>
-        <div class="text-gray-600 -mt-4">Quelle: <a href="https://www.e-recht24.de" target="_blank" rel="noopener noreferrer">eRecht24</a></div>
+
+        <div class="my-4">
+          <h2>Kontakt</h2>
+          Telefon: 02509-2094947<br>
+          E-Mail: linus@tebbe.dev
         </div>
-    </div>
+
+        <div class="my-4">
+          <h2>Umsatzsteuer-ID</h2>
+          UStID. gemäß §27a UStG.: DE326934606
+        </div>
+
+        <div class="my-4">
+          <h2>Streitschlichtung</h2>
+          Die Europ&auml;ische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit: <a href="https://ec.europa.eu/consumers/odr" class="hover:underline" target="_blank" rel="noopener noreferrer">https://ec.europa.eu/consumers/odr</a>.<br />
+          Unsere E-Mail-Adresse finden Sie oben im Impressum.<br>
+          <br>
+          Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.
+        </div>
+
+        <div class="my-4">
+          <h2>Haftung für Inhalte</h2>
+          Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich.<br>
+          Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.<br>
+          Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt.<br>
+          Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich.<br>
+          Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
+        </div>
+
+        <div class="my-4">
+          <h2>Haftung für Links</h2>
+          Unser Angebot enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben.<br>
+          Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen.<br>
+          Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich.<br>
+          Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft.<br>
+          Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.<br>
+          Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar.<br>
+          Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen
+        </div>
+
+        <div class="my-4">
+          <h2>Urheberrecht</h2>
+          Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht.<br>
+          Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers.<br>
+          Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.<br>
+          Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet.<br>
+          Insbesondere werden Inhalte Dritter als solche gekennzeichnet.<br>
+          Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis.<br>
+          Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.<br>
+        </div>
+    </Card>
 </template>
 
 
